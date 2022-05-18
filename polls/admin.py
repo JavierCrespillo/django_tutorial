54d6b6788b99ebd9da32417ba115e20484869dd8
JavierCrespillo <crespilloberguaj@gmail.com>
2022-05-18 13:48:42 +0200

    Entorno de desarrollo terminado

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Choice, Question
+from .models import Choice, Question, Categoria
+
+
 
 
 class ChoiceInline(admin.TabularInline):
@@ -21,3 +23,5 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Categoria)
+
